8e14c52c0c22dc8c211ee37e343882e3089e9c98
DepressiveCat <monstersapbe@gmail.com>
2024-11-13 21:43:36 +0000

    fix: tuve problemas metiendo el Punto_B en un sh por temas de permisos entonces que cree el archivo /usr/local/bin/<tu-apellido>AltaUser-Groups.sh en el dir /tmp/ y dsp lo pase a /usr/local/bin/ usando sudo

--- RTA_Examen_20241112/Punto_B.sh
+++ RTA_Examen_20241112/Punto_B.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-cat << EOF > /usr/local/bin/ToscanoAltaUser-Groups.sh
+cat << 'EOF' > /tmp/ToscanoAltaUser-Groups.sh
 #!/bin/bash
 
 USUARIO_REF=$1
 LISTA=$2
 
-CONTRA=$(sudo grep $USUARIO_REF /etc/shadow | awk -F ':' '{print $2}')
+CONTRA=$(sudo grep "$USUARIO_REF" /etc/shadow | awk -F ":" '{print $2}')
 
 ANT_IFS=$IFS
 IFS=$'\n'
-for i in `cat $LISTA | awk -F ',' '{print $1" "$2" "$3}' | grep -v ^#`; do
-        USUARIO=$(echo $i | awk '{print$1}')
-        GRUPO=$(echo $i | awk '{print$2}')
-        HOME_USR=$(echo $i | awk '{print$3}')
-        sudo groupadd -f $GRUPO
-        sudo useradd -m -d $HOME_USR -g $GRUPO -p $CONTRA $USUARIO
+for i in $(cat "$LISTA" | awk -F "," '{print $1" "$2" "$3}' | grep -v ^#); do
+    USUARIO=$(echo "$i" | awk '{print $1}')
+    GRUPO=$(echo "$i" | awk '{print $2}')
+    HOME_USR=$(echo "$i" | awk '{print $3}')
+    sudo groupadd -f "$GRUPO"
+    sudo useradd -m -d "$HOME_USR" -g "$GRUPO" -p "$CONTRA" "$USUARIO"
 done
 IFS=$ANT_IFS
 EOF
 
-sudo chmod 744 /usr/local/bin/ToscanoAltaUser-Groups.sh
+sudo mv /tmp/ToscanoAltaUser-Groups.sh /usr/local/bin/ToscanoAltaUser-Groups.sh
+sudo chmod 755 /usr/local/bin/ToscanoAltaUser-Groups.sh
+
 /usr/local/bin/ToscanoAltaUser-Groups.sh toscano /home/toscano/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
