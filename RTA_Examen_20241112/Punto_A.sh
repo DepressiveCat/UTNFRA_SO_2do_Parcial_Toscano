7b0328bdc1a13e172756d50115682a210299a851
DepressiveCat <monstersapbe@gmail.com>
2024-11-13 04:15:06 +0000

    feat: se agrega el comando solicitado en Punto_A.sh

---
+++ RTA_Examen_20241112/Punto_A.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir /work
+
+sudo fdisk /dev/sdd
+n
+
+
+
++5M
+n
+
+
+
++512M
+w
+sudo fdisk /dev/sdc
+n
+
+
+
++1.5G
+w
+
+sudo pvcreate /dev/sdd1
+sudo vgcreate vg_datos /dev/sdd1
+sudo pvcreate /dev/sdc1
+sudo vgextend vg_datos /dev/sdc1
+sudo pvcreate /dev/sdd2
+sudo vgcreate vg_temp /dev/sdd2
+
+sudo lvcreate -L +4M vg_datos -n lv_docker
+sudo lvcreate -l +100%FREE vg_datos -n lv_workareas
+sudo lvcreate -l +100%FREE vg_temp -n lv_swap
+
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkfs.ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkswap /dev/vg_temp/lv_swap
+
+sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
+sudo mount /dev/mapper/vg_datos-lv_workareas /work/
+sudo swapon /dev/vg_temp/lv_swap
